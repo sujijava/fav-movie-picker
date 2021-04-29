129565ab00ab2d50ff4c02e7ac5288474b632944
susieoh8@gmail.com <59837559+sujijava@users.noreply.github.com>
2021-04-29 18:02:09 -0400

    deploy ready

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-/** deploy
+// deploy
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
@@ -13,7 +13,7 @@ app.use(bodyParser.json({}))
 app.use(bodyParser.urlencoded({}))
 app.use(cors())
 
-app.use('/contacts', listRoutes)
+app.use('/lists', listRoutes)
 
 //Serve static assets if in production
 // if (process.env.NODE_ENV === 'production') {
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
 // }
 
 const CONNECTION_URL =
-  'mongodb+srv://dbUser:admin123123@cluster0.g3lyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  'mongodb+srv://admin:admin123123@cluster0.cgyal.mongodb.net/mernPRJ?retryWrites=true&w=majority'
 
 const PORT = process.env.PORT || 5000
 
@@ -41,9 +41,8 @@ mongoose
 
 mongoose.set('useFindAndModify', false)
 
-**/
-
 //development
+/***
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
@@ -77,3 +76,4 @@ mongoose
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port`)))
   .catch((error) => console.log(`${error} did not connect`))
 mongoose.set('useFindAndModify', false)
+**/
